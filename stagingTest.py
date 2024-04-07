6ee64043183daf06b9dfa73e3bd6cee3a2a7aa46
Aks2812 <77261121+Aks2812@users.noreply.github.com>
2024-04-07 15:02:37 +0700

    14th commit

--- stagingTest.py
+++ stagingTest.py
@@ -38,6 +38,7 @@ def init_db():
                     iframe_count INTEGER,
                     is_obfuscated INTEGER,
                     redirected_url TEXT,
+                    is_https INTEGER,
                     phishing_chance TEXT
                 )
             ''')
@@ -152,13 +153,18 @@ def detect_obfuscated(url):
     
 def detect_url_redirect(url):
     try:
+        # Send a request to the URL without allowing redirects
         response = requests.get(url, allow_redirects=False)
+        
+        # Check for server-side redirection (HTTP status codes 301 and 302)
         if response.status_code == 301 or response.status_code == 302:
-            redirected_url = response.headers['Location']
-            return redirected_url
+            return response.headers['Location']  # Return the redirected URL
+        
+        # Check for client-side redirection (JavaScript-based)
         else:
             html_code = response.text
-            # Define regular expressions to search for URL redirection patterns
+            
+            # Define regular expressions to search for URL redirection patterns in HTML content
             patterns = [
                 r'http-equiv\s*=\s*"refresh"\s*content\s*=\s*["\']\d+;\s*url\s*=\s*([^"\']+)["\']',
                 r'window\.location\s*=\s*["\']([^"\']+)["\']',
@@ -166,15 +172,53 @@ def detect_url_redirect(url):
                 r'window\.location\.href\s*=\s*["\']([^"\']+)["\']',
                 r'http\.open\s*[(][^,]+,\s*["\']([^"\']+)["\']'
             ]
+            
             # Search for patterns in the HTML code
             for pattern in patterns:
                 match = re.search(pattern, html_code)
                 if match:
                     return match.group(1)  # Return the redirected URL if found
+                    
             return None  # Return None if no redirection patterns are found
+            
     except Exception as e:
         print("Error:", e)
         return None
+
+# def detect_url_redirect(url):
+#     try:
+#         response = requests.get(url, allow_redirects=False)
+#         if response.status_code == 301 or response.status_code == 302:
+#             redirected_url = response.headers['Location']
+#             return redirected_url
+#         else:
+#             html_code = response.text
+#             # Define regular expressions to search for URL redirection patterns
+#             patterns = [
+#                 r'http-equiv\s*=\s*"refresh"\s*content\s*=\s*["\']\d+;\s*url\s*=\s*([^"\']+)["\']',
+#                 r'window\.location\s*=\s*["\']([^"\']+)["\']',
+#                 r'window\.location\.replace\s*[(]["\']([^"\']+)["\'][)]',
+#                 r'window\.location\.href\s*=\s*["\']([^"\']+)["\']',
+#                 r'http\.open\s*[(][^,]+,\s*["\']([^"\']+)["\']'
+#             ]
+#             # Search for patterns in the HTML code
+#             for pattern in patterns:
+#                 match = re.search(pattern, html_code)
+#                 if match:
+#                     return match.group(1)  # Return the redirected URL if found
+#             return None  # Return None if no redirection patterns are found
+#     except Exception as e:
+#         print("Error:", e)
+#         return None
+    
+# Function to check if URL is using HTTPS
+def is_https(url):
+    try:
+        response = requests.get(url)
+        return response.url.startswith('https')
+    except Exception as e:
+        print(f"Error checking HTTPS: {e}")
+        return False
 
 # Process just 1 URL 
 # def process_message(cursor, sender_number, message):
@@ -210,41 +254,92 @@ def detect_url_redirect(url):
 #     else:
 #         return message + " bukan sebuah URL valid, Silahkan masukan URL yang valid."
 
-# # Process 2 URLs at Once with Comma Value 
+# Process 2 URLs at Once with Comma Value V1.0
+# def process_message(cursor, sender_number, message):
+#     if contains_url(message):
+#         urls = [url.strip() for url in message.split(',')]
+
+#         responses = []
+#         for url in urls:
+#             phishing_urls = load_phishing_urls()
+
+#             # Check if the input URL matches any variation in the phishing database
+#             if url in phishing_urls:
+#                 list_based_response = f"\nURL {url} terdeteksi merupakan phishing pada database kami.\n"
+#             else:
+#                 # Check if the input URL with a trailing slash matches any variation in the phishing database
+#                 message_with_slash = url + '/'
+#                 if message_with_slash in phishing_urls:
+#                     list_based_response = f"\nURL {url} terdeteksi merupakan phishing pada database kami.\n"
+#                 else:
+#                     save_message_if_not_exists(cursor, sender_number, url)
+#                     cursor.execute("SELECT timestamp, count FROM messages WHERE message_body=?", (url,))
+#                     result = cursor.fetchone()
+#                     if result:
+#                         timestamp, count = result
+#                         cursor.execute("UPDATE messages SET timestamp=CURRENT_TIMESTAMP, count=? WHERE message_body=?", (count + 1, url))
+#                         cursor.connection.commit()
+#                         list_based_response = f"\nURL {url} tidak ditemukan dalam database phishing kami.\nTerakhir dilaporkan pada {timestamp} sebanyak {count} kali.\n"
+#                     else:
+#                         list_based_response = "0"
+
+#             rule_based_response, phishing_chance = check_and_save_rulebased(url)
+#             iframe_count = count_iframes(url)
+#             if iframe_count > 0:
+#                 rule_based_response += f" Jumlah Iframe 🖼️: {iframe_count}\n"
+#             combined_response = f"{list_based_response} {rule_based_response} Kemungkinan phishing 🚩: {phishing_chance}"
+#             responses.append(combined_response)
+        
+#         return '\n'.join(responses)
+#     else:
+#         return message + " bukan sebuah URL valid, Silahkan masukan URL yang valid."
+
+# Process 2 URLs at Once with Comma Value V2.0
 def process_message(cursor, sender_number, message):
     if contains_url(message):
         urls = [url.strip() for url in message.split(',')]
-
         responses = []
+
         for url in urls:
-            phishing_urls = load_phishing_urls()
-
-            # Check if the input URL matches any variation in the phishing database
-            if url in phishing_urls:
-                list_based_response = f"URL {url} terdeteksi merupakan phishing pada database kami.\n"
+            db = get_db()
+            cursor.execute("SELECT * FROM rulebased WHERE url=?", (url,))
+            result = cursor.fetchone()
+            
+            if result:
+                # URL already exists in rulebased table, return previous analysis results
+                rule_based_response = result[1]  # Assuming the response is stored in the second column
+                phishing_chance = result[11]  # Assuming the phishing chance is stored in the third column
+                
+                response_message = f"\nURL {rule_based_response} telah dilaporkan sebelumnya dan memiliki kemungkinan sebagai URL phishing sebesar 🚩: {phishing_chance}"
             else:
-                # Check if the input URL with a trailing slash matches any variation in the phishing database
-                message_with_slash = url + '/'
-                if message_with_slash in phishing_urls:
-                    list_based_response = f"URL {url} terdeteksi merupakan phishing pada database kami.\n"
-                else:
-                    save_message_if_not_exists(cursor, sender_number, url)
-                    cursor.execute("SELECT timestamp, count FROM messages WHERE message_body=?", (url,))
-                    result = cursor.fetchone()
-                    if result:
-                        timestamp, count = result
-                        cursor.execute("UPDATE messages SET timestamp=CURRENT_TIMESTAMP, count=? WHERE message_body=?", (count + 1, url))
-                        cursor.connection.commit()
-                        list_based_response = f"URL {url} tidak ditemukan dalam database phishing kami.\nTerakhir dilaporkan pada {timestamp} sebanyak {count} kali.\n"
-                    else:
-                        list_based_response = "0"
+                phishing_urls = load_phishing_urls()
 
-            rule_based_response, phishing_chance = check_and_save_rulebased(url)
-            iframe_count = count_iframes(url)
-            if iframe_count > 0:
-                rule_based_response += f" Jumlah Iframe 🖼️: {iframe_count}\n"
-            combined_response = f"{list_based_response} {rule_based_response} Kemungkinan phishing 🚩: {phishing_chance}"
-            responses.append(combined_response)
+                if url in phishing_urls:
+                    list_based_response = f"\nURL {url} terdeteksi merupakan phishing pada database kami.\n"
+                else:
+                    message_with_slash = url + '/'
+                    if message_with_slash in phishing_urls:
+                        list_based_response = f"\nURL {url} terdeteksi merupakan phishing pada database kami.\n"
+                    else:
+                        save_message_if_not_exists(cursor, sender_number, url)
+                        cursor.execute("SELECT timestamp, count FROM messages WHERE message_body=?", (url,))
+                        result = cursor.fetchone()
+                        if result:
+                            timestamp, count = result
+                            cursor.execute("UPDATE messages SET timestamp=CURRENT_TIMESTAMP, count=? WHERE message_body=?", (count + 1, url))
+                            db.commit()
+                            list_based_response = f"\nURL {url} tidak ditemukan dalam database phishing kami.\nTerakhir dilaporkan pada {timestamp} sebanyak {count} kali.\n"
+                        else:
+                            list_based_response = "0"
+
+                rule_based_response, phishing_chance = check_and_save_rulebased(url)
+                iframe_count = count_iframes(url)
+                if iframe_count > 0:
+                    rule_based_response += f" Jumlah Iframe 🖼️: {iframe_count}\n"
+                combined_response = f"{list_based_response} {rule_based_response} Kemungkinan phishing 🚩: {phishing_chance}"
+                response_message = combined_response
+            
+            responses.append(response_message)
         
         return '\n'.join(responses)
     else:
@@ -338,9 +433,10 @@ def check_and_save_rulebased(url):
         has_password_field = check_password_field(url)
         iframe_count = count_iframes(url)
         is_obfuscated = detect_obfuscated(url)
-        redirected_url = detect_url_redirect(url) 
+        redirected_url = detect_url_redirect(url)
+        https = is_https(url) 
 
-        criteria_met = 8 - sum([
+        criteria_met = 9 - sum([
             is_common_tld != "Unknown",
             not is_cyrillic,
             contains_special_characters,
@@ -348,17 +444,18 @@ def check_and_save_rulebased(url):
             not has_password_field,
             not iframe_count,
             is_obfuscated,
-            1 if redirected_url else 0
+            1 if redirected_url else 0,
+            1 if https else 0
         ])
-        phishing_chance = "100%" if criteria_met == 8 else "50%" if criteria_met >= 4 else "28%" if criteria_met >= 2 else "25%" if criteria_met >= 1 else "URL tidak terdeteksi phishing"
+        phishing_chance = "100%" if criteria_met == 9 else "44%" if criteria_met >= 4 else "22%" if criteria_met >= 2 else "11%" if criteria_met >= 1 else "URL tidak terdeteksi phishing"
 
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("INSERT INTO rulebased (url, tld, ssl_ver, is_cyrillic, has_submit_button, has_password_field, iframe_count, is_obfuscated, redirected_url, phishing_chance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-            (url, 'Common' if is_common_tld else 'Uncommon', ssl_version, 1 if is_cyrillic else 0, 1 if has_submit_button else 0, 1 if has_password_field else 0, iframe_count, 1 if is_obfuscated else 0, '301' if redirected_url else '302', phishing_chance))
+        cursor.execute("INSERT INTO rulebased (url, tld, ssl_ver, is_cyrillic, has_submit_button, has_password_field, iframe_count, is_obfuscated, redirected_url, is_https, phishing_chance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+            (url, 'Common' if is_common_tld else 'Uncommon', ssl_version, 1 if is_cyrillic else 0, 1 if has_submit_button else 0, 1 if has_password_field else 0, iframe_count, 1 if is_obfuscated else 0, '301' if redirected_url else '302', 1 if is_https else 0, phishing_chance))
         db.commit()
 
-        rule_based_response = rule_based_response = f"-----------------------\n 🔍 Hasil Analisa 🔍 \n -----------------------\n URL 🔗 : {url}\n Top Level Domain (TLD) 🌐: {'Umum' if is_common_tld else 'Tidak Umum'}\n SSL Version 🔒 : {ssl_version}\n Karakter Cyrillic 🆎 : {'Mengandung Karakter Cyrillic' if is_cyrillic else 'Tidak Mengandung Karakter Cyrillic'}\n Terdapat Spesial Karakter❗: {'Ya' if contains_special_characters else 'Tidak'}\n Submit Button 📥: {'Terdeteksi' if has_submit_button else 'Tidak Terdeteksi'}\n Password Field 🔑: {'Terdeteksi' if has_password_field else 'Tidak Terdeteksi'}\n Iframe 🖼️: {'Terdeteksi' if iframe_count else 'Tidak Terdeteksi'}\n Obsfuscated: {'Ya' if is_obfuscated else 'Tidak'}\n Redirect: {'Ya' if redirected_url else 'Tidak'}\n" 
+        rule_based_response = rule_based_response = f"-----------------------\n 🔍 Hasil Analisa 🔍 \n -----------------------\n URL 🔗 : {url}\n Top Level Domain (TLD) 🌐: {'Umum' if is_common_tld else 'Tidak Umum'}\n SSL Version 🔒 : {ssl_version}\n Karakter Cyrillic 🆎 : {'Mengandung Karakter Cyrillic' if is_cyrillic else 'Tidak Mengandung Karakter Cyrillic'}\n Terdapat Spesial Karakter❗: {'Ya' if contains_special_characters else 'Tidak'}\n Submit Button 📥: {'Terdeteksi' if has_submit_button else 'Tidak Terdeteksi'}\n Password Field 🔑: {'Terdeteksi' if has_password_field else 'Tidak Terdeteksi'}\n Iframe 🖼️: {'Terdeteksi' if iframe_count else 'Tidak Terdeteksi'}\n Obsfuscated: {'Ya' if is_obfuscated else 'Tidak'}\n Redirect: {'Ya' if redirected_url else 'Tidak'}\n HTTPS: {'Ya' if https else 'Tidak'}\n" 
         return rule_based_response, phishing_chance
     except Exception as e:
         print(f"Terjadi kesalahan dalam melakukan analisis: {e}")
